7b88758ffcc91a9e061194901842b50daf2b1162
이영현 <leey9494@icloud.com>
2019-05-20 15:15:01 +0900

    [MOD] 사칙연산 계산기 enum 공부하기 위해 수정

--- src/main/java/operation/Operation.java
+++ src/main/java/operation/Operation.java
@@ -1,42 +1,20 @@
 package operation;
 
-import java.util.regex.Pattern;
+import static operation.OperationValidator.checkDivideByZero;
+import static operation.OperationValidator.isNumber;
 
 public class Operation {
-    public int calculate(String index) {
-        String[] valueArr = index.split(" ");
-        int result = Integer.parseInt(valueArr[0]);
+    double calculate(String predictor) {
+        String[] predictorIndex = predictor.trim().split(" ");
+        double result = isNumber(predictorIndex[0]);
 
-        for (int i = 0; i < valueArr.length; i++) {
-            result = getResult(valueArr, result, i);
+        for (int i = 1; i < predictorIndex.length; i += 2) {
+            double number = isNumber(predictorIndex[i + 1]);
+            String operator = checkDivideByZero(predictorIndex[i], number);
+            result = Operator.doCalculator(operator, result, number);
         }
 
-        return result;
-    }
-
-    private int getResult(String[] valueArr, int result, int i) {
-        String patterns = "^[0-9]$";
-
-        if (!Pattern.matches(patterns, valueArr[i])) {
-            result = operate(valueArr, result, i);
-
-        }
-        return result;
-    }
-
-    private int operate(String[] valueArr, int result, int i) {
-        if (valueArr[i].equals("+")) {
-            result += Integer.parseInt(valueArr[i + 1]);
-        }
-        if (valueArr[i].equals("-")) {
-            result -= Integer.parseInt(valueArr[i + 1]);
-        }
-        if (valueArr[i].equals("*")) {
-            result *= Integer.parseInt(valueArr[i + 1]);
-        }
-        if (valueArr[i].equals("/")) {
-            result /= Integer.parseInt(valueArr[i + 1]);
-        }
         return result;
     }
 }
+
--- src/test/java/operation/OperationTest.java
+++ src/test/java/operation/OperationTest.java
@@ -5,9 +5,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class OperationTest {
-    Operation operation;
+    private Operation operation;
 
     @BeforeEach
     void setUp() {
@@ -17,6 +18,26 @@ public class OperationTest {
     @Test
     void 계산() {
         assertThat(operation.calculate("2 + 3 * 4 / 2")).isEqualTo(10);
+    }
+
+    @Test
+    void 다항식_연산자_위치_및_문자열이_올_경우() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            operation.calculate("+ 2 / 3");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            operation.calculate("2 + @ + 3");
+        });
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+            operation.calculate("3 + 2 *");
+        });
+    }
+
+    @Test
+    void 영으로_나눌_경우() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            operation.calculate("2 / 0");
+        });
     }
 
     @AfterEach
